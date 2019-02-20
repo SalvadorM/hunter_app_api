fe97b1daf1c5e681209e09676080a02bb17f59ec
SalvadorM <sronikle@gmail.com>
2019-02-20 11:51:12 -0500

    change class to course (var name)

--- controllers/course.js
+++ controllers/course.js
@@ -3,39 +3,43 @@ const db = require('../config/database')
 
 //class model 
 const Course = db.Course
+const User = db.User
 
 
-const classController = {
-    classRouter(){
+const courseController = {
+    courseRouter(){
         const router = express.Router()
 
         //routes for class model 
-        router.get('/find/:id', this.getClassById)
-        router.post('/add', this.addClass)
+        router.get('/find/:id', this.getCourseById)
+        router.post('/add', this.addCourse)
         return router
     },
 
     //@route    GET /find/:id
     //@params   id
     //@desc     Get class information by id from database
-    getClassById(req, res){
+    getCourseById(req, res){
         const classId = req.params.id
-        Class.findByPk(classId)
-            .then(classFound => {
-                res.json(classFound)
+        Course.findByPk(classId)
+            .then(course => {
+                res.json(course)
             })
             .catch(err => res.status(400).json(err))
     },
 
     //@route    POST /add
     //@desc     POST a class to database
-    addClass(req, res){
+    addCourse(req, res){
         const classCode = req.body.classCode 
         const className = req.body.className
         const section = req.body.section 
         const information = req.body.information 
+        // after user auth userId = req.user.id
+        const userId = req.body.userId
+        const semesterId = req.body.semester
 
-        Course.create({classCode, className, section, information})
+        Course.create({classCode, className, section, information, userId, semesterId})
             .then(addedClass => {
                 res.json(addedClass)
             })
@@ -43,4 +47,4 @@ const classController = {
     }
 }
 
-module.exports = classController.classRouter()
+module.exports = courseController.courseRouter()
